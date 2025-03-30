3f8d8b44d853a68699bb8fd0b02c742512871484
Chakrit <chakrit.t@arise.tech>
2025-03-31 00:07:01 +0700

    setup

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "myuser"
+	password = "mypassword"
+	dbname   = "mydatabase"
+)
+
+func main() {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	print(db)
+	fmt.Println("Connect Successful!")
+}
---
+++ models.go
@@ -0,0 +1,9 @@
+package main
+
+type Book struct {
+	ID          int64
+	Name        string
+	Authur      string
+	Description string
+	Price       string
+}
